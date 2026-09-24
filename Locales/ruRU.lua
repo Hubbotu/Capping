2d8c6451d1ac9a64123c12e0ece154546b97d451
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-09-24 07:28:49 +0300

    Update ruRU.lua

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -2,57 +2,57 @@
 if GetLocale() ~= "ruRU" then return end
 local _, mod = ...
 local L = mod.L
+-- Translator ZamestoTV
+L.battleBegins = "Битва начинается"
+L.finalScore = "Итог: %d - %d"
+L.flagRespawns = "Появление флага"
+L.timeRemaining = "Оставшееся время"
 
-L.battleBegins = "Начало сражения"
-L.finalScore = "Финал: %d - %d"
-L.flagRespawns = "Появление Флагов"
-L.timeRemaining = "Осталось"
+L.takenTheFlagTrigger = "^(.+) приносит флаг!"
+L.hasTakenTheTrigger = "берет"
+L.droppedTrigger = "роняет"
+L.capturedTheTrigger = "захватывает"
 
-L.takenTheFlagTrigger = "^(.+) захватывает флаг!"
-L.hasTakenTheTrigger = "захватил"
-L.droppedTrigger = "уронил"
-L.capturedTheTrigger = "захватил"
-
-L.hordeGate = "Врата крепости Орды"
-L.allianceGate = "Врата крепости Альянса"
-L.gatePosition = "%s (%s)" -- тут точно нужен перевод?// check on live
-L.west = "Запад"
-L.front = "Передовая" -- тут еще вопрос (!) проверить на проде // spellcheck on live
-L.east = "Восток"
+L.hordeGate = "Врата Орды"
+L.allianceGate = "Врата Альянса"
+L.gatePosition = "%s (%s)"
+L.west = "Западные"
+L.front = "Передние"
+L.east = "Восточные"
 L.hordeBoss = "Босс Орды"
 L.allianceBoss = "Босс Альянса"
-L.galvangar = "Гальвангар" -- Капитан Гальвангар <Капитан клана Северного Волка>
-L.balinda = "Балинда" -- Капитан Балинда Каменный Очаг <Капитан клана Грозовой Вершины>
-L.ivus = "Ивус" -- Ивус Лесной Властелин
-L.lokholar = "Локолар" -- Локолар Владыка Льда
-L.handIn = "|cFF33FF99Capping|r: Автоматическая сдача квестовых предметов." -- проверить на проде // spellcheck on live
-L.anchorTooltip = "|cffeda55fПравый клик|r открыть настройки"
---L.anchorTooltipNote = "Open the options and lock the bars to hide this moving anchor."
+L.galvangar = "Гальвангар"
+L.balinda = "Балинда"
+L.ivus = "Ивус"
+L.lokholar = "Локолар"
+L.handIn = "|cFF33FF99Захват|r: автоматическая сдача квестовых предметов."
+L.anchorTooltip = "|cffeda55fПКМ|r: открыть настройки"
+L.anchorTooltipNote = "Откройте настройки и зафиксируйте панели, чтобы скрыть этот якорь перемещения."
 
 --- Alliance IoC Workshop yells:
---Гном-механик кричит: Я уже почти закончил! Только не подпускай ко мне Орду – в инженерной школе не учат махать мечом!
---Гном-механик кричит: Уже сломалась? Не о чем беспокоиться. Я могу починить что угодно.
+-- Gnomish Mechanic yells: I'm halfway there! Keep the Horde away from here.  They don't teach fighting in engineering school!
+-- Gnomish Mechanic yells: It's broken already?! No worries. It's nothing I can't fix.
 --- Horde IoC Workshop yells:
---Гоблинский механик кричит: я на полпути! Держите Альянс подальше - боевые действия не входят в мой контракт!
---Механик Гоблин кричит: «Он снова сломан ?!» Я исправлю это ... просто не ожидайте, что гарантия покроет это.
-L.halfway = " по[^ ]+ти[! ]"
-L.broken = "слома"
+-- Goblin Mechanic yells: I'm about halfway done! Keep the Alliance away - fighting's not in my contract!
+-- Goblin Mechanic yells: It's broken again?! I'll fix it... just don't expect the warranty to cover this.
+L.halfway = "наполовину" -- Needs to match the in game text exactly
+L.broken = "разрушены" -- Needs to match the in game text exactly
 
 -- Wintergrasp
-L.damaged = "|cFF33FF99Capping|r: %s подвергается нападению" -- проверить на проде, подумать над универсальным вариантом // spellcheck on live
-L.destroyed = "|cFF33FF99Capping|r: %s разрушена" -- проверить на проде, подумать над универсальным вариантом // spellcheck on live
+L.damaged = "|cFF33FF99Захват|r: объект %s поврежден"
+L.destroyed = "|cFF33FF99Захват|r: объект %s разрушен"
 L.northEastKeep = "Северо-восточная башня крепости"
 L.southEastKeep = "Юго-восточная башня крепости"
 L.northWestKeep = "Северо-западная башня крепости"
 L.southWestKeep = "Юго-западная башня крепости"
 L.northWest = "Северо-западная стена"
 L.southWest = "Юго-западная стена"
-L.south = "Юная стена"
+L.south = "Южная стена"
 L.southEast = "Юго-восточная стена"
 L.northEast = "Северо-восточная стена"
 L.innerWest = "Внутренняя западная стена"
 L.innerSouth = "Внутренняя южная стена"
-L.innerEast = "Внутренняя востовная стена"
+L.innerEast = "Внутренняя восточная стена"
 L.southGate = "Южные ворота"
 L.mainEntrance = "Главный вход"
 L.westTower = "Западная башня"
@@ -60,13 +60,13 @@ L.southTower = "Южная башня"
 L.eastTower = "Восточная башня"
 
 -- Ashran
---L.hordeGuardian = "Horde Guardian"
---L.allianceGuardian = "Alliance Guardian"
+L.hordeGuardian = "Страж Орды"
+L.allianceGuardian = "Страж Альянса"
 L.kronus = "Крон"
 L.fangraal = "Фанграал"
 
 -- Arena
---L.arenaStartTrigger = "The Arena battle has begun!" -- Needs to match the in game text exactly
---L.arenaStart60s = "One minute until the Arena battle begins!" -- Needs to match the in game text exactly
---L.arenaStart30s = "Thirty seconds until the Arena battle begins!" -- Needs to match the in game text exactly
---L.arenaStart15s = "Fifteen seconds until the Arena battle begins!" -- Needs to match the in game text exactly
+L.arenaStartTrigger = "Бой на Арене начался!" -- Needs to match the in game text exactly
+L.arenaStart60s = "Одна минута до начала боя на Арене!" -- Needs to match the in game text exactly
+L.arenaStart30s = "Тридцать секунд до начала боя на Арене!" -- Needs to match the in game text exactly
+L.arenaStart15s = "Пятнадцать секунд до начала боя на Арене!" -- Needs to match the in game text exactly
